642205daa00a83e9823c67bc7834b22f028d3671
Bradley Pagano <bpagano5243@gmail.com>
2019-11-19 19:26:29 -0500

    adds starter data to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,25 @@
-var friendList = [];
+var friendList = [
+    {
+        "name": "Rick",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        "name": "Morty",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [1, 1, 5, 3, 2, 4, 3, 5, 3, 2]
+    },
+    {
+        "name": "Slade",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [1, 2, 4, 5, 1, 5, 3, 5, 3, 2]
+    },
+    {
+        "name": "Wade",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [1, 4, 5, 3, 4, 5, 3, 5, 3, 2]
+    },
+];
 
 
 module.exports = friendList;
