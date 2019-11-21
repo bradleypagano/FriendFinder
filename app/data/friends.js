63f85b4723b6841152b150254a4cba2803fb30e7
Bradley Pagano <bpagano5243@gmail.com>
2019-11-21 16:19:25 -0500

    fixes backend code and adds images to the modal popup

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 var friendList = [
     {
         "name": "Rick",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://vignette.wikia.nocookie.net/rickandmorty/images/8/8f/Rickk22.png/revision/latest?cb=20170911060616",
         "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     },
     {
         "name": "Morty",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://vignette.wikia.nocookie.net/rickandmorty/images/4/41/Morty_Smith.jpg/revision/latest?cb=20170217193441",
         "scores": [1, 1, 5, 3, 2, 4, 3, 5, 3, 2]
     },
     {
         "name": "Slade",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://hobbydb-production.s3.amazonaws.com/processed_uploads/subject_photo/subject_photo/image/11071/1456351007-7425-2524/Screen_20Shot_202016-02-24_20at_202.56.10_20PM_large.png",
         "scores": [1, 2, 4, 5, 1, 5, 3, 5, 3, 2]
     },
     {
         "name": "Wade",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://thebottomline.as.ucsb.edu/wp-content/uploads/2018/05/Icaro-Deadpool.jpg",
         "scores": [1, 4, 5, 3, 4, 5, 3, 5, 3, 2]
     },
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,29 @@
 let friendsList = require("../data/friends");
 
-module.exports = function(app){
-    app.get("/api/friends", function(req, res){
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
         res.json(friendsList);
     });
-    
-    app.post("/api/friends", function(req, res){
+
+    app.post("/api/friends", function (req, res) {
+        let bestMatch;
+        let bestScore = 100;
+        console.log(req.body)
+
+        let newScore = req.body.scores.reduce((a, b) => a + parseInt(b), 0);
+
+        for (i = 0; i < friendsList.length; i++) {
+            let fScore = friendsList[i].scores.reduce((a, b) => a + parseInt(b), 0);
+            const diff = Math.abs(newScore - fScore);
+            console.log(diff, diff < bestScore)
+            if (diff < bestScore) {
+                bestScore = diff;
+                bestMatch = friendsList[i];
+            };
+            console.log(bestMatch);
+        };
         friendsList.push(req.body);
-        res.json(true);
+        res.json(bestMatch);
     });
 
 };
